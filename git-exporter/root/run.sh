dee71a9c798443a6e62b829af605a8f45ce46d94
Lukas Baege <lukas.baege@gmail.com>
2022-09-19 18:11:55 +0200

    bugfix

--- git-exporter/root/run.sh
+++ git-exporter/root/run.sh
@@ -149,7 +149,7 @@ function export_addons {
     done
     bashio::log.info "Get addon repositories"
     bashio::api.supervisor GET "/store/repositories" false \
-        | jq '. | map(select(.source != null and .source != "core" and .source != "local")) | map({(.name): {source,maintainer,slug}}) | add' > /tmp/tmp.json'
+        | jq '. | map(select(.source != null and .source != "core" and .source != "local")) | map({(.name): {source,maintainer,slug}}) | add' > /tmp/tmp.json
     /utils/jsonToYaml.py /tmp/tmp.json
     mv /tmp/tmp.yaml "/tmp/addons/repositories.yaml"
     rsync -archive --compress --delete --checksum --prune-empty-dirs -q /tmp/addons/ ${local_repository}/addons
